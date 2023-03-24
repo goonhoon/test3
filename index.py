c8fc35a79f2456eb1ad0cb1b5092cccbe63b4559
Goonhoon <56773206+goonhoon@users.noreply.github.com>
2023-03-24 19:13:43 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import Flask,url_for,render_template,request
 import spacy
 from spacy import displacy
-nlp = spacy.load("../models/model-best")
+nlp = spacy.load("models/model-best")
 
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
 
